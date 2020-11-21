5d92f096907e3ab1306956ddeeec28a578461e7d
tezz-io <matblockgreninja@gmail.com>
2020-11-21 11:29:53 +0530

    Initial Commit

---
+++ TymTebal/TymTebalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TymTebalApp.swift
+//  TymTebal
+//
+//  Created by Tejas M R on 21/11/20.
+//
+
+import SwiftUI
+
+@main
+struct TymTebalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
